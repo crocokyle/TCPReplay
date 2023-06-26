1f44846dd6ba796627b4050c5c62f99610a33052
Kyle Dixon <kdixon@verveindustrial.com>
2023-06-26 12:36:27 -0400

    :recycle: Refactor import module

--- import.py
+++ import.py
@@ -1,16 +1,60 @@
 from pathlib import Path
 from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP, TCP
+from scapy.packet import Packet
 from scapy.utils import PcapNgReader
 from typing import Optional
 
 
-class TCPPacket:
-    def __init__(self, packet):
-        self.packet = packet
-        self.ip_layer = packet.getlayer(IP)
-        self.tcp_layer = packet.getlayer(TCP)
-        self.l2_layer = packet.getlayer(Ether)
+class PacketCapture:
+    def __init__(self, filepath: Path, client_ip: str = '', server_ip: str = ''):
+        self.filepath: Path = filepath
+        self.packets: list[TCPPacket] = list()
+        self.client_ip: str = client_ip
+        self.server_ip: str = server_ip
+
+    def decode_pcapng(self):
+        if self.filepath.name[-6:] != "pcapng":
+            raise ValueError(f'Provided file: {self.filepath} is not a .pcapng file.')
+
+        pcap_reader = PcapNgReader(filename=str(self.filepath))
+        self.packets = pcap_reader.read_all().res
+        output_dict = dict()
+
+        for i, packet in enumerate(self.packets):
+            # Make sure we ignore other devices in the pcap by filtering out other IPs
+            if (packet.ip_layer.src == self.client_ip or packet.ip_layer.src == self.server_ip) and \
+                    (packet.ip_layer.dst == self.client_ip or packet.ip_layer.dst == self.server_ip):
+                packet.direction = 'request' if packet.ip_layer.src == self.client_ip else 'response'
+
+            # We can try to auto-detect the client/server by looking at who sent/received the SYN...this is probably bad
+            if not self.client_ip and str(packet.flags) == 'S':
+                self.client_ip = packet.ip_layer.src
+                self.server_ip = packet.ip_layer.dst
+
+            # We're creating an entry in the dictionary that contains the request data as the key,
+            # the "time_until_response" and "response" data as sub-values
+            output_dict[i] = {
+                "time_until_response": packet,
+                "response": packet
+            }
+            # print(block._raw) #byte type raw data
+
+
+class TCPPacket(Packet):
+    """
+    Extends scapy.packet.Packet
+    """
+    def __init__(self):
+        super(TCPPacket, self).__init__()
+        self.ip_layer = self.getlayer(IP)
+        self.tcp_layer = self.getlayer(TCP)
+        self.l2_layer = self.getlayer(Ether)
+
+        self.direction: str = ''
+
+    def decode_packet(self):
+        pass
 
 
 def import_folder(directory: Path):
@@ -18,35 +62,6 @@ def import_folder(directory: Path):
     for filename in files:
         decoded_dict = decode_pcap(filename)
         #save_json(decoded_dict)
-
-
-def decode_pcap(filename) -> dict:
-    pcap_reader = PcapNgReader(filename=str(filename))
-    packets = pcap_reader.read_all().res
-    output_dict = dict()
-    client_ip: Optional[str] = None
-    server_ip: Optional[str] = None
-
-    for i, packet in enumerate(packets):
-        # We determine who is a client/server by looking at who sent/received the SYN...this is probably bad
-        if str(tcp_pkt.flags) == 'S':
-            client_ip = ip_pkt.getlayer(IP).src
-            server_ip = ip_pkt.getlayer(IP).dst
-
-        if client_ip and server_ip:
-            # Make sure we ignore other devices in the pcap
-            if (ip_pkt.src == client_ip or ip_pkt.src == server_ip) and \
-                    (ip_pkt.dst == client_ip or ip_pkt.dst == server_ip):
-
-                p_type = 'request' if ip_pkt.src == client_ip else 'response'
-
-                # We're creating an entry in the dictionary that contains the request data as the key,
-                # the "time_until_response" and "response" data as subvalues
-                output_dict[i] = {
-                    "time_until_response": packet,
-                    "response": packet
-                }
-                # print(block._raw) #byte type raw data
 
 
 if __name__ == '__main__':
